640e4c80ae088d08f71b65fafb88554726662dca
Gülseren <gulseren7k@gmail.com>
2023-03-06 15:50:45 +0300

    Hour1 Tamamlandı

---
+++ hour1/Hour1.h
@@ -0,0 +1,26 @@
+#ifndef HOUR1_H
+#define HOUR1_H
+
+
+#include <iostream>
+
+
+
+// Hour1
+
+
+void MerhabaDunya(){
+
+    std::cout << "Merhaba Dunya" << std::endl;
+
+}
+
+
+
+
+
+
+
+
+
+#endif // HOUR1_H
